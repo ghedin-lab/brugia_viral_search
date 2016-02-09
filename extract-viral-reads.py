d8fb04ca284d8bcf6c15e8ace83862d266d87a68
twaddlac <twaddlac@gmail.com>
2016-02-09 14:57:36 -0500

    bug fix

--- extract-viral-reads.py
+++ extract-viral-reads.py
@@ -10,7 +10,6 @@ with open("kraken.translate.out", "rb") as kraken:
 		l = line.split("\t")
 		if 'Viruses' in l[1] and 'phiX' not in l[1]:
 			v[l[0]] = "v"
-			print line
 			
 with open("kraken.translate.out", "rb") as kraken:
 	for line in kraken:
@@ -22,16 +21,10 @@ with open("all-unmapped.r1.fastq", "rb") as f:
 	with open("all-unmapped.r1.viral.fastq", "w") as out:
 		for record in SeqIO.parse(f, "fastq"):
 			if record.id in v:
-				if v[record.id] == "v":
-					out.write(record.format("fastq"))
-			else:
 				out.write(record.format("fastq"))
 
 with open("all-unmapped.r2.fastq", "rb") as f:
 	with open("all-unmapped.r2.viral.fastq", "w") as out:
 		for record in SeqIO.parse(f, "fastq"):
 			if record.id in v:
-				if v[record.id] == "v":
-					out.write(record.format("fastq"))
-			else:
 				out.write(record.format("fastq"))
