f4a09ad2e400039d9a72ef6074cda2db31a396d5
twaddlac <twaddlac@gmail.com>
2016-02-13 15:23:14 -0500

    bug fix

--- extract-paired-reads.py
+++ extract-paired-reads.py
@@ -24,7 +24,7 @@ with open(sys.argv[2],"rb") as r2:
 			seqs[m.group(1)]["r1"] = "None"
 			
 
-with open(sys.argv[3], "w") as r1, open(sys.argv[3], "w") as r2, open(sys.argv[3], "w") as se:
+with open(sys.argv[3], "w") as r1, open(sys.argv[4], "w") as r2, open(sys.argv[5], "w") as se:
 	for name in seqs:
 		if seqs[name]["r1"] == "None":
 			se.write(seqs[name]["r1"])
