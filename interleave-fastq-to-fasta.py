4b861dac0598dd53eababb9a866a03a9078afd88
twaddlac <twaddlac@gmail.com>
2016-02-16 12:16:44 -0500

    added interleave script and more docs

---
+++ interleave-fastq-to-fasta.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python
+
+import sys
+
+with open(sys.argv[1], "rb") as r1, open(sys.argv[2],"rb") as r2, open(sys.argv[3],"w") as out:
+	while(True):
+		line = r1.readline()
+		if line.strip() == "":
+			break
+		print line.strip()
+		print r1.readline().strip()
+		
+		for i in xrange(2):
+			r1.readline().strip()
+		
+		
+		for i in xrange(2):
+			print r2.readline().strip()
+			
+		for i in xrange(2):
+			r2.readline().strip()
