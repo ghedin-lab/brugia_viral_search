24fcef1a75323671aef252ab73a39d0eb3eee269
twaddlac <twaddlac@gmail.com>
2016-02-16 12:20:34 -0500

    interleave prints to std out

--- interleave-fastq-to-fasta.py
+++ interleave-fastq-to-fasta.py
@@ -2,7 +2,7 @@
 
 import sys
 
-with open(sys.argv[1], "rb") as r1, open(sys.argv[2],"rb") as r2, open(sys.argv[3],"w") as out:
+with open(sys.argv[1], "rb") as r1, open(sys.argv[2],"rb") as r2:
 	while(True):
 		line = r1.readline()
 		if line.strip() == "":
