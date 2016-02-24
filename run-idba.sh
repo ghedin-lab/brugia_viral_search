7a8b16532f49c637306b02510f8f2dcb9967a7b5
twaddlac <twaddlac@gmail.com>
2016-02-24 12:28:10 -0500

    pckaged scripts and notebook together, separate from the data

--- run-idba.sh
+++ run-idba.sh
@@ -9,4 +9,8 @@ module load idba
 
 cd $path
 
-idba_id --pre_correction --num_threads 20 -r $fasta -o $fasta.idba.d
+idba_id --pre_correction --num_threads 20 -l $fasta -o $fasta.idba.d
+
+cd $fasta.idba.d
+
+blastn -db /scratch/at120/shared/db/blast/nt/nt -query contig-100.fa -out contig-100.blastn.xml -outfmt 5 -culling_limit 2 -max_target_seqs 1 -num_threads 20 -evalue 0.00005
